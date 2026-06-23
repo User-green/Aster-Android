2ab499ec6cebb89ea032ad3639fd240a540712b5
Lucas <257867858+findleyl@users.noreply.github.com>
2026-06-23 14:34:00 -0400

    chore: bump version to 0.6.83

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -29,8 +29,8 @@ android {
         applicationId = "org.astermail.android"
         minSdk = 26
         targetSdk = 35
-        versionCode = 91
-        versionName = "0.6.82"
+        versionCode = 92
+        versionName = "0.6.83"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables { useSupportLibrary = true }
